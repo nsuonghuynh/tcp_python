442fef1e7c21a1dbe97aadf3205a8ad48005a602
Suong Huynh <bez15@txstate.edu>
2024-03-05 19:36:17 -0600

    case1 done

--- client.py
+++ client.py
@@ -2,36 +2,77 @@ import socket
 import sys
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--ip", "-i", type=str, default='localhost',help="Server IP")
-parser.add_argument("--port", "-p", type=int, default=8000, help="Server port")
-args = parser.parse_args()
 
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# Bind the socket
-# server_address = ('localhost', 8000)
-server_address = (args.ip, args.port)
-sock.connect(server_address)
-
-try:
+def menu():
+    print("Menu")
+    print("1. Add new student")
+    print("2. Display Sudent Details by ID")
+    print("3. Display Student Details by Score")
+    print("4. Display All Student Data")
+    print("5. Delete Student Details")
+    print("6. Exit")
     
-    # Send data
-    message = 'This is the message.  It will be repeated.'
-    print(f'sending "{message}"', file=sys.stderr)
-    encoded_message = message.encode('utf-8')
-    sock.sendall(encoded_message)
 
-    # Look for the response
-    amount_received = 0
-    amount_expected = len(message)
-    
-    while amount_received < amount_expected:
-        data = sock.recv(16)
-        amount_received += len(data)
-        print(f'received "{data}"', file=sys.stderr)
+def main():
 
-finally:
-    print('closing socket', file=sys.stderr)
-    sock.close()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ip", "-i", type=str, default='localhost',help="Server IP")
+    parser.add_argument("--port", "-p", type=int, default=8000, help="Server port")
+    args = parser.parse_args()
+
+    # Create a TCP/IP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Bind the socket
+    server_address = ('localhost', 8000)
+
+    # server_address = (args.ip, args.port)
+    sock.connect(server_address)
+
+    try:
+        while True:
+            # Print out menu
+            menu()
+            choice = input("Enter your choice: ")
+            if choice == 6:
+                exit()
+
+            if (choice == '1'):
+
+                id = input("Enter student ID: ")
+                lname = input("Enter student last name:")
+                fname = input("Enter student first name:")
+                score = input("Enter student score:")
+
+                items = [choice, id, lname, fname, score]
+                message = " ".join(items)
+
+                print(f'Sending "{message}"', file=sys.stderr)
+                encoded_message = message.encode('utf-8')
+                sock.sendall(encoded_message)
+
+            # Send data
+            # message = choice
+            print(f'Sending "{message}"', file=sys.stderr)
+            encoded_message = message.encode('utf-8')
+            sock.sendall(encoded_message)
+
+            # Look for amount received
+            data = sock.recv(1024)
+            print(f'Received "{data.decode('utf-8')}"', file=sys.stderr)
+
+            # # Look for the response
+            # amount_received = 0
+            # amount_expected = len(message)
+            
+            # while amount_received < amount_expected:
+            #     data = sock.recv(16)
+            #     amount_received += len(data)
+            #     print(f'received "{data}"', file=sys.stderr)
+
+    finally:
+        print('closing socket', file=sys.stderr)
+        sock.close()
+
+if __name__ == '__main__':
+    main()
--- server.py
+++ server.py
@@ -2,42 +2,128 @@ import socket
 import sys
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--ip", "-i", type=str, default='localhost',help="Server IP")
-parser.add_argument("--port", "-p", type=int, default=8000, help="Server port")
-args = parser.parse_args()
+student_list = []
 
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+class Student:
+    def __init__(self, id, lname, fname, score):
+        self.id = id
+        self.lname = lname
+        self.fname = fname
+        self.score = score
 
-# Bind the socket
-# server_address = ('localhost', 8000)
-server_address = (args.ip, args.port)
+    def __str__(self):
+        return f"ID: {self.id}, Last: {self.lname}, First: {self.fname}, Score: {self.score}\n"
+    
+    def update_id(self, new_id):
+        self.id = new_id
 
-sock.bind(server_address)
+    def update_lname(self, new_lname):
+        self.lname = new_lname
+    
+    def update_fname(self, new_fname):
+        self.fname = new_fname
 
-# Listen for incoming connections
-sock.listen(1)
+    def update_score(self, new_score):
+        self.score = new_score
 
-while True:
-    # Wait for a connection
-    print('waiting for a connection', file=sys.stderr)
-    connection, client_address = sock.accept()
+def case1(connection):
 
-    try:
-        
-        # Receive the data in small chunks and retransmit it
-        while True:
-            data = connection.recv(16)
-            decoded_data = data.decode('utf-8')
-            print(f'received "{decoded_data}"', file=sys.stderr)
-            if data:
-                print('sending data back to the client', file=sys.stderr)
-                connection.sendall(data)
-            else:
-                print(f'no more data from {client_address}' , file=sys.stderr)
-                break
+    # Receive student data from the client
+    data = connection.recv(1024).decode()
+    print("Received for case1:", data)
+
+    items = data.split()
+
+    id = items[1]
+    lname = items[2]
+    fname = items[3]
+    score = items[4]
+    
+    new_student = Student(id, lname, fname, score)
+    student_list.append(new_student)
+
+    with open("db.txt", "a") as file:
+        file.write(new_student.__str__())
+    
+    return "Added new student"
+
+def case2():
+    return "case2"
+
+def case3():
+    return "case3"
+
+def case4():
+    pass
+
+def case5():
+    pass
+
+def exit_program():
+    pass
+
+def switch_case(choice, connection):
+    switcher = {
+        '1': case1,
+        '2': case2,
+        '3': case3,
+        '4': case4,
+        '5': case5,
+        '6': exit_program
+    }
+    func = switcher.get(choice, lambda: "Invalid choice")
+    return func(connection)
+    
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ip", "-i", type=str, default='localhost',help="Server IP")
+    parser.add_argument("--port", "-p", type=int, default=8000, help="Server port")
+    args = parser.parse_args()
+
+
+    # Create a TCP/IP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Bind the socket
+    # server_address = ('localhost', 8000)
+    server_address = (args.ip, args.port)
+
+    sock.bind(server_address)
+
+    # Listen for incoming connections
+    sock.listen(1)
+
+    while True:
+        # Wait for a connection
+        print('waiting for a connection', file=sys.stderr)
+        connection, client_address = sock.accept()
+
+        try:
             
-    finally:
-        # Clean up the connection
-        connection.close()
+            # Receive the data in small chunks and retransmit it
+            while True:
+                
+                data = connection.recv(1024).decode('utf-8')
+
+                print(data)
+
+                if data:
+                    if data == '6':
+                        break
+                    response = switch_case(data[0], connection)
+                    connection.sendall(response.encode('utf-8'))
+                    
+                # print(f'Received "{decoded_data}"', file=sys.stderr)
+                # if data:
+                #     print('sending data back to the client', file=sys.stderr)
+                #     connection.sendall(data)
+                # else:
+                #     print(f'No more data from {client_address}' , file=sys.stderr)
+                #     break
+                
+        finally:
+            # Clean up the connection
+            connection.close()
+
+if __name__ == "__main__":
+    main()
